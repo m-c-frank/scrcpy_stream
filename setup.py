800a2a16968062f990b146073f9c7d9dfe760fb0
m-c-frank <martin7.frank7@gmail.com>
2022-01-06 11:54:50 +0100

    License

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ setuptools.setup(
     description="A small package for easily and continuously capturing screenshots from scrcpy",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/m-c-frank/scrcpy_stream/",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://github.com/m-c-frank/scrcpy_stream/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
